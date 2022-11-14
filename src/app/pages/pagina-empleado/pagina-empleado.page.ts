96589fe0303491bc0b339b79a6abf73e20fd3403
PanConQueso0 <101284386+PanConQueso0@users.noreply.github.com>
2022-11-14 13:35:38 -0300

    Pagina empleado con las distintas opciones 🤑🤑🤑🤑

--- src/app/pages/pagina-empleado/pagina-empleado.page.ts
+++ src/app/pages/pagina-empleado/pagina-empleado.page.ts
@@ -7,8 +7,25 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./pagina-empleado.page.scss'],
 })
 export class PaginaEmpleadoPage implements OnInit {
+  u: string = "";
+  p: string = "";
+  a: boolean = false;
+  n: string = "";
+  f: string = "";
+  r: string = "";
+  fe: string = "";
 
   constructor(private router: Router, private activedRouter: ActivatedRoute) {
+    this.activedRouter.queryParams.subscribe(params =>{
+      if(this.router.getCurrentNavigation().extras.state){
+        this.u = this.router.getCurrentNavigation().extras.state.user;
+        this.p = this.router.getCurrentNavigation().extras.state.pass;
+        this.a = this.router.getCurrentNavigation().extras.state.afil;
+        this.n = this.router.getCurrentNavigation().extras.state.name;
+        this.f = this.router.getCurrentNavigation().extras.state.fono;
+        this.r = this.router.getCurrentNavigation().extras.state.rut;
+      }
+    })
     
    }
 
